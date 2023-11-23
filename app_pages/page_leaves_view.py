9f7e4a1813287abd2cc4e54e753f9579725f8d84
Diegomoro415 <>
2023-11-23 23:02:01 +0000

    finish content leaves_view dashboard

--- app_pages/page_leaves_view.py
+++ app_pages/page_leaves_view.py
@@ -8,45 +8,87 @@ import seaborn as sns
 import itertools
 import random
 
+
 def page_leaves_view_body():
+    st.write("# Data Visualizer")
+    st.info(
+        "Data Visualizer, where we delve into the intricate details"
+        " of cherry leaves."
+        " Our study focuses on visually differentiating leaves affected"
+        " by powdery mildew from healthy ones."
+    )
 
-    st.write("### Leaves View")
+    st.write("## Study Overview")
+    st.success(
+        "In this research, our primary objective is to pinpoint visual"
+        " indicators of powdery mildew infection on cherry leaves. Early signs"
+        " often manifest as circular lesions with a subtle green tint,"
+        " eventually giving rise to a delicate, cottony appearance"
+        " manifests in the infected regions.\n\n"
 
-# Checkbox for the user to visualize the difference between the average and variability of the images
-    if st.checkbox("Difference between average and variability of the image"):
+        "Transforming these visual traits into the language of machine"
+        " learning necessitates meticulous image preparation before initiating"
+        " model training. This preparatory step is crucial for optimal"
+        " feature extraction and effective training.\n\n"
+
+        "When handling an image dataset, the imperative lies in the"
+        " normalization of images before the Neural Network training process."
+        " Normalization entails determining the mean and standard deviation"
+        " across the entire dataset, employing a mathematical algorithm"
+        " tailored to the inherent qualities of each image.\n\n"
+
+        "This normalization process significantly enhances the model's"
+        " capacity to comprehend and generalize from the dataset,"
+        " thereby elevating overall performance."
+    )
+
+    st.write("For additional details and guidelines, please refer to the Project "
+             "[Project README](https://github.com/Diegomoro415/mildew_detection/tree/main)")
+
+    # Using expanders for each section
+    with st.beta_expander("Difference between average and variability image"):
         version = 'v1'
         avg_var_healthy = plt.imread(f"outputs/{version}/avg_var_healthy.png")
-        avg_var_powdery_mildew = plt.imread(f"outputs/{version}/avg_var_powdery_mildew.png")
+        avg_var_powdery_mildew = plt.imread(
+            f"outputs/{version}/avg_var_powdery_mildew.png")
 
         st.warning(
-            "Note that the average and variability images did not show distinct patterns. "
-            "However, a slight difference in color pigment is observed in the average images for both classes."
+            "Note that the average and variability images did not show distinct"
+            " patterns. "
+            "However, a slight difference in color pigment is observed in the"
+            " average images for both classes."
         )
 
-        st.image(avg_var_healthy, caption='Healthy Leaf - Average and Variability')
-        st.image(avg_var_powdery_mildew, caption='Powdery Mildew Leaf - Average and Variability')
+        st.image(avg_var_healthy,
+                 caption='Healthy Leaf - Average and Variability')
+        st.image(avg_var_powdery_mildew,
+                 caption='Powdery Mildew Leaf - Average and Variability')
         st.write("---")
 
-    # Checkbox for the user to visualize the difference between the average images of healthy and powdery mildew leaves
-    if st.checkbox("Differences between average healthy and average powdery mildew leaves"):
+    with st.beta_expander(
+            "Differences between average healthy and average infected leaves"):
         avg_diff = plt.imread(f"outputs/{version}/avg_diff.png")
 
         st.warning(
-            "Note that this study didn't show distinct patterns where we could intuitively differentiate one from another."
+            "Note that this study didn't show distinct patterns where we"
+            " could intuitively differentiate one from another."
         )
         st.image(avg_diff, caption='Difference between average images')
 
-    # Checkbox for the user to create an image montage
-    if st.checkbox("Image Montage"):
-        st.write("* To refresh the montage, click on the 'Create Montage' button")
+    with st.beta_expander("Image Montage"):
+        st.write(
+            "* To refresh the montage, click on the 'Create Montage' button"
+        )
         my_data_dir = 'inputs/cherryleaves_dataset/cherry-leaves'
         labels = os.listdir(my_data_dir + '/validation')
-        label_to_display = st.selectbox(label="Select class", options=labels, index=0)
+        label_to_display = st.selectbox(
+            label="Select class", options=labels, index=0)
         if st.button("Create Montage"):
             image_montage(dir_path=my_data_dir + '/validation',
-                           label_to_display=label_to_display,
-                           nrows=3, ncols=3, figsize=(10, 10))
+                          label_to_display=label_to_display,
+                          nrows=3, ncols=3, figsize=(10, 10))
         st.write("---")
+
 
 # Function to create the image montage
 def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15, 10)):
@@ -86,6 +128,7 @@ def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15, 10)):
     else:
         st.warning("The selected class does not exist.")
         st.warning(f"The existing options are: {labels}")
+
 
 # Run the Streamlit code
 if __name__ == '__main__':
