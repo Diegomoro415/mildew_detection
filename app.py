866784a7ff569801eac56464ae5e738558d3f53a
Diegomoro415 <>
2023-11-25 16:01:24 +0000

    create ml_performance page dashboard to show how the dataset was divided and how the model performed

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from app_pages.page_summary import page_summary_body
 from app_pages.page_leaves_view import page_leaves_view_body
 from app_pages.page_mildew_detector import page_mildew_detector_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
-from app_pages.page_ml_performance import page_ml_performance_metrics
+from app_pages.page_ml_performance import page_ml_performance_dashboard
 
 app = MultiPage(app_name="Mildew Detection")  # Create an instance of the app
 
@@ -15,6 +15,6 @@ app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Leaves View", page_leaves_view_body)
 app.add_page("Powdery Mildew detector", page_mildew_detector_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
-app.add_page("ML Performance Metrics", page_ml_performance_metrics)
+app.add_page("ML Performance Metrics", page_ml_performance_dashboard)
 
 app.run()  # Run the app
--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -1,6 +1,100 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.image import imread
+from src.machine_learning.evaluate_clf import load_test_evaluation
 
 
-def page_ml_performance_metrics():
+def page_ml_performance_dashboard():
+    version = 'v1'
 
-    st.write("### ML Performance Metricy")
+    st.info(
+        f"This dashboard provides a user-friendly presentation of how the"
+        f" dataset was divided and how the model performed on that data."
+    )
+
+    # Section: Images Distribution
+    st.write("## Images Distribution per Set and Label")
+
+    st.warning(
+        f"The leaves dataset was divided into three subsets:\n\n"
+        f"- **Train set (70%):** Initial data used to train the model.\n"
+        f"- **Validation set (10%):** Used to fine-tune the model.\n"
+        f"- **Test set (20%):** Unseen data for final model evaluation."
+    )
+
+    labels_distribution_img = plt.imread(
+        f"outputs/{version}/labels_distribution.png")
+    st.image(labels_distribution_img,
+             caption='Labels Distribution on Train, Validation, and Test Sets',
+             width=500)
+
+    sets_distribution_img = plt.imread(
+        f"outputs/{version}/sets_distribution.png")
+    st.image(sets_distribution_img, caption='Sets Distribution', width=500)
+
+    st.write("---")
+
+    # Section: Model Performance
+    st.write("## Model Performance")
+
+    # Classification Report
+    st.warning(
+        f"**Classification Report:**\n\n"
+        f"Precision, Recall, F1 Score, and Support provide insights into"
+        f" model performance for each class."
+    )
+
+    clf_report_img = plt.imread(f"outputs/{version}/c_report.png")
+    st.image(clf_report_img, caption='Classification Report', width=500)
+
+    # ROC Curve
+    st.warning(
+        f"**ROC Curve:**\n\n"
+        f"ROC curve illustrates the model's ability to distinguish between"
+        f" classes."
+        f" AUC (Area Under the Curve) measures overall model performance."
+    )
+
+    roc_curve_img = plt.imread(f"outputs/{version}/roc_curve.png")
+    st.image(roc_curve_img, caption='ROC Curve', width=500)
+
+    # Confusion Matrix
+    st.warning(
+        f"**Confusion Matrix:**\n\n"
+        f"Confusion Matrix helps evaluate classifier performance, showing"
+        f" true positive/negative and false positive/negative."
+    )
+
+    confusion_matrix_img = plt.imread(
+        f"outputs/{version}/confusion_matrix.png")
+    st.image(confusion_matrix_img, caption='Confusion Matrix', width=500)
+
+    # Model Performance Plots
+    st.warning(
+        f"**Model Performance:**\n\n"
+        f"Loss and Accuracy plots depict the model's learning progress"
+        f" during training."
+    )
+
+    model_losses_img = plt.imread(
+        f"outputs/{version}/model_training_losses.png")
+    st.image(model_losses_img, caption='Model Training Losses', width=500)
+
+    model_acc_img = plt.imread(f"outputs/{version}/model_training_acc.png")
+    st.image(model_acc_img, caption='Model Training Accuracy', width=500)
+
+    st.write("---")
+
+    # Section: Generalized Performance on Test Set
+    st.write("## Generalized Performance on Test Set")
+
+    test_evaluation_df = pd.DataFrame(
+        load_test_evaluation(version), index=['Loss', 'Accuracy'])
+    st.dataframe(test_evaluation_df)
+
+    st.write(
+        f"For additional information, please visit and **read** the "
+        f"[Project README file]"
+        f"(https://github.com/yourusername/yourproject#readme)."
+    )
